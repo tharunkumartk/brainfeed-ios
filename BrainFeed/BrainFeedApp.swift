4fe84c6d769a11715402c0ac088830e7d88384d5
Tharun Kumar <tharun.tiruppali@gmail.com>
2024-09-21 15:05:03 -0400

    Initial Commit

---
+++ BrainFeed/BrainFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainFeedApp.swift
+//  BrainFeed
+//
+//  Created by Tharun Kumar on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct BrainFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
